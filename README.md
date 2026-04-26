b7efcb7ad5d980bc92bec5ea4652a68e35a1a6b2
Mouaad020 <mouaad.chakroun@hva.nl>
2026-04-26 12:04:49 +0200

    #6 set up

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
+# Interactive Functionality
 
-# Enhanced website
-Ontwerp en maak een interactieve website die snel laadt en prettig te gebruiken is.
+Ontwerp en maak voor een opdrachtgever een interactieve toepassing die voor iedereen toegankelijk is
 
-De instructie vind je in: [INSTRUCTIONS.md](https://github.com/fdnd-task/enhanced-website/blob/main/docs/INSTRUCTIONS.md)
+De instructie vind je in: [INSTRUCTIONS.md](https://github.com/fdnd-task/the-web-is-for-everyone-interactive-functionality/blob/main/docs/INSTRUCTIONS.md)
 
 
 ## Inhoudsopgave
@@ -16,17 +16,18 @@ De instructie vind je in: [INSTRUCTIONS.md](https://github.com/fdnd-task/enhance
 
 ## Beschrijving
 <!-- Bij Beschrijving staat kort beschreven wat voor project het is en wat je hebt gemaakt -->
-<!-- Voeg een mooie poster visual toe 📸 -->
-<!-- Voeg een link toe naar je live site 🌐-->
+<!-- Voeg een mooie poster visual of video toe 📸 -->
+<!-- Voeg een link toe naar GitHub Pages 🌐-->
 
 ## Gebruik
 <!-- Bij Gebruik staat de user story, hoe het werkt en wat je er mee kan. -->
 
 ## Kenmerken
-<!-- Bij Kenmerken staat welke technieken zijn gebruikt en hoe. Wat is de HTML structuur? Wat zijn de belangrijkste dingen in CSS? Wat is er met JS gedaan en hoe? Misschien heb je iets met NodeJS gedaan, of heb je een framwork of library gebruikt? -->
+<!-- Bij Kenmerken staat welke technieken zijn gebruikt en hoe. Wat is de HTML structuur? Wat zijn de belangrijkste dingen in CSS? Wat is er met JS gedaan en hoe? Misschien heb je iets met NodeJS gedaan, of heb je een framework of library gebruikt? -->
 
 ## Installatie
 <!-- Bij Installatie staat hoe een andere developer aan jouw repo kan werken -->
+
 
 ## Bronnen
 
--- server.js
+++ server.js
@@ -1,5 +1,131 @@
-console.log('Hier komt je server voor Sprint 10.')
+// Importeer het npm package Express (uit de door npm aangemaakte node_modules map)
+// Deze package is geïnstalleerd via `npm install`, en staat als 'dependency' in package.json
+import express from 'express'
 
-console.log('Gebruik uit Sprint 9 alleen de code die je mee wilt nemen.')
+// Importeer de Liquid package (ook als dependency via npm geïnstalleerd)
+import { Liquid } from 'liquidjs';
 
-console.log('Zet \'m op!')
+// Maak een nieuwe Express applicatie aan, waarin we de server configureren
+const app = express()
+
+// Maak werken met data uit formulieren iets prettiger
+app.use(express.urlencoded({ extended: true }))
+
+// Gebruik de map 'public' voor statische bestanden (resources zoals CSS, JavaScript, afbeeldingen en fonts)
+// Bestanden in deze map kunnen dus door de browser gebruikt worden
+app.use(express.static('public'))
+
+// Stel Liquid in als 'view engine'
+const engine = new Liquid()
+app.engine('liquid', engine.express())
+
+// Stel de map met Liquid templates in
+// Let op: de browser kan deze bestanden niet rechtstreeks laden (zoals voorheen met HTML bestanden)
+app.set('views', './views')
+
+
+console.log('Let op: Er zijn nog geen routes. Voeg hier dus eerst jouw GET en POST routes toe.')
+
+
+// Zie https://expressjs.com/en/5x/api.html#app.get.method over app.get()
+
+
+  // Zie https://expressjs.com/en/5x/api.html#res.render over response.render()
+
+
+
+  app.get('/', async function (request, response) {
+  const snapmapsResponse = await fetch('https://fdnd-agency.directus.app/items/snappthis_snapmap?fields=*,snaps.*,snaps.actions.*')
+  const snapmapsJSON = await snapmapsResponse.json()
+  const snapmaps = snapmapsJSON.data
+
+  response.render('index.liquid', { snapmaps })
+})
+
+
+app.get('/snapmap/:slug', async function (request, response) {
+  const slug = request.params.slug
+
+  const snapmapResponse = await fetch(`https://fdnd-agency.directus.app/items/snappthis_snapmap?filter[slug][_eq]=${slug}&fields=*,snaps.*`)
+  const snapmapJSON = await snapmapResponse.json()
+  const snapmap = snapmapJSON.data[0]
+  const snaps = snapmap.snaps
+
+  response.render('snapmap.liquid', { snapmap, snaps })
+})
+
+app.get('/snap/:uuid', async function (request, response) {
+  const uuid = request.params.uuid
+
+  const snapResponse = await fetch(`https://fdnd-agency.directus.app/items/snappthis_snap/${uuid}?fields=*,actions.*`)
+  const snapJSON = await snapResponse.json()
+  const snap = snapJSON.data
+
+  response.render('snap.liquid', { snap })
+})
+  
+
+app.post('/snap/:uuid/like', async function (request, response) {
+  const uuid = request.params.uuid
+
+  await fetch('https://fdnd-agency.directus.app/items/snappthis_action', {
+    method: 'POST',
+    body: JSON.stringify({
+      action: 'like',
+      snap: uuid
+    }),
+    headers: {
+      'Content-Type': 'application/json;charset=UTF-8'
+    }
+  })
+
+   response.redirect(303, '/')
+ 
+})
+
+
+
+/*
+// Zie https://expressjs.com/en/5x/api.html#app.post.method over app.post()
+app.post(…, async function (request, response) {
+
+  // In request.body zitten alle formuliervelden die een `name` attribuut hebben in je HTML
+  console.log(request.body)
+
+  // Via een fetch() naar Directus vullen we nieuwe gegevens in
+
+  // Zie https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch over fetch()
+  // Zie https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/JSON/stringify over JSON.stringify()
+  // Zie https://docs.directus.io/reference/items.html#create-an-item over het toevoegen van gegevens in Directus
+  // Zie https://docs.directus.io/reference/items.html#update-an-item over het veranderen van gegevens in Directus
+  const fetchResponse = await fetch(…, {
+    method: …,
+    body: JSON.stringify(…),
+    headers: {
+      'Content-Type': 'application/json;charset=UTF-8'
+    }
+  })
+
+  // Als de POST niet gelukt is, kun je de response loggen. Sowieso een goede debugging strategie.
+  // console.log(fetchResponse)
+
+  // Eventueel kun je de JSON van die response nog debuggen
+  // const fetchResponseJSON = await fetchResponse.json()
+  // console.log(fetchResponseJSON)
+
+  // Redirect de gebruiker daarna naar een logische volgende stap
+  // Zie https://expressjs.com/en/5x/api.html#res.redirect over response.redirect()
+  response.redirect(303, …)
+})
+*/
+
+
+// Stel het poortnummer in waar Express op moet gaan luisteren
+// Lokaal is dit poort 8000; als deze applicatie ergens gehost wordt, waarschijnlijk poort 80
+app.set('port', process.env.PORT || 8000)
+
+// Start Express op, gebruik daarbij het zojuist ingestelde poortnummer op
+app.listen(app.get('port'), function () {
+  // Toon een bericht in de console
+  console.log(`Daarna kun je via http://localhost:${app.get('port')}/ jouw interactieve website bekijken.\n\nThe Web is for Everyone. Maak mooie dingen 🙂`)
+})
